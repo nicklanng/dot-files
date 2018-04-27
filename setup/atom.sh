dbeebbb6578d94cd56a6cdde8f0ac6829a306bfe
Nick Lanng <rinse@rinseworld.com>
2018-04-27 14:06:34 +0100

    Create atom.sh

---
+++ setup/atom.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+apm install / 
+busy-signal /
+file-icons /
+go-debug /
+go-find-references /
+go-outline /
+go-plus /
+go-rename /
+go-signature-statusbar /
+hyperclick /
+intentions /
+language-babel /
+language-bnf /
+language-diff /
+language-docker /
+language-hcl /
+language-protobuf /
+linter /
+linter-js-standard /
+linter-travis-lint /
+linter-ui-default /
+markdown-preview-plus /
+minimap /
+pigments /
+platformio-ide-terminal /
+seti-syntax /
+seti-ui /
+split-diff /
+standard-formatter /
+tablr /
+teletype
+
